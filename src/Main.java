c0ac85beb6a1663b894a86c93f45ff331d6d6650
Jose <alfonsotobonperegrina@gmail.com>
2026-05-03 16:16:48 -0600

    Primer programa Java - Hola Mundo - Inicio ruta a Microsoft

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+void main() {
+    //VARIABLES
+    String nombre;
+    //CREACION DE SCANNER
+    Scanner teclado = new Scanner(System.in);
+    System.out.println("Cual es tu nombre?");
+    nombre = teclado.nextLine();
+    System.out.println("Hola " + nombre + " bienvenido seas al camino a Microsoft!");
+    }
+
