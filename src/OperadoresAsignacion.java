f2436246f5771d2d2f59c1d8f154a768a3349f88
Jose <alfonsotobonperegrina@gmail.com>
2026-06-19 20:29:59 -0600

    Add assignment operators examples with String concatenation

---
+++ src/OperadoresAsignacion.java
@@ -0,0 +1,24 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args){
+        int i = 5;/*el operador de asignacion es = asigna el valor a una variable*/
+        System.out.println("i = " + i);
+        int j = i + 4;
+        System.out.println("j = i + 4 --> " + j);
+        i += 2;// i = i + 2; <-- suma escrita de una manera mas simplificada
+        System.out.println("i += 2 --> " + i);
+        i += 5;// i = i + 5; <-- suma escrita de una manera mas simplificada
+        System.out.println("i += 5 --> " + i);
+        j -= 4;// j = j - 4; <-- resta escrita de una manera mas simplificada
+        System.out.println("j -= 4 --> " + j);
+        i *= 2;// i = i * 2; <-- multiplicasion escrita de una manera mas simplificada
+        System.out.println("i *= 2 --> " + i);
+        i /= 4;// i = i / 4; <-- divicion escrita de una manera mas simplificada
+        System.out.println("i /= 4 --> " + i);
+        i %= j;// i = i % j; <-- mod escrita de una manera mas simplificada
+        System.out.println("i %= j --> " + i);
+        String cadena = "Hola";
+        cadena += " ";
+        cadena += "Mundo";
+        System.out.println("cadena = " + cadena);
+    }
+}
