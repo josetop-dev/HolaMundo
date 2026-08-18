f8ee7944ed9c1ee15c7f5af97c34fea0e2f2b3b7
Jose <alfonsotobonperegrina@gmail.com>
2026-08-17 23:21:49 -0600

    Add switch-case month name display with input validation

--- src/SentenciaSwitchCase.java
+++ src/SentenciaSwitchCase.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args){
         int num = 1;
@@ -45,5 +47,67 @@ public class SentenciaSwitchCase {
             default:
                 System.out.println("Caracter desconocido");
         }
+
+        //MOSTRAR MES
+        Scanner keyboard = new Scanner(System.in);
+        String mes = null;
+        boolean b1 = true;
+        int numMes = 0;
+        System.out.println("Ingrese el numero del mes...");
+        while (b1) {
+            try {
+                numMes = keyboard.nextInt();
+                if(numMes<=12 && numMes>=1){
+                    b1 = false;
+                }else {
+                    System.out.println("Ingrese un numero entre 1 & 12...");
+                }
+            } catch (Exception e) {
+                System.out.println("Ingresa un numero entre 1 & 12...");
+                keyboard.nextLine();
+            }
+        }
+        switch (numMes){
+            case 1:
+                mes = "Enero";
+                break;
+            case 2:
+                mes = "Febrero";
+                break;
+            case 3:
+                mes = "Marzo";
+                break;
+            case 4:
+                mes = "Abril";
+                break;
+            case 5:
+                mes = "Mayo";
+                break;
+            case 6:
+                mes = "Junio";
+                break;
+            case 7:
+                mes = "Julio";
+                break;
+            case 8:
+                mes = "Agosto";
+                break;
+            case 9:
+                mes = "Septiembre";
+                break;
+            case 10:
+                mes = "Octubre";
+                break;
+            case 11:
+                mes = "Noviembre";
+                break;
+            case 12:
+                mes = "Diciembre";
+                break;
+//  Como desde la entrada se asegura que el caso exista no hay necesidad de esto:
+//          default:
+//                mes = "Indefnido";
+        }
+        System.out.println("mes = " + mes);
     }
 }
